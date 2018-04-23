c3e0b970b771f229c2f03bd4dc7404b503c050f9
github username <github email address>
2018-04-23 18:13:53 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,6 +1,38 @@
 require_relative 'config/environment'
+require 'pry'
 
 class App < Sinatra::Base
-  # Write your code here!
+  get '/reversename/:name' do
+    params[:name].reverse
+  end
 
+  get '/square/:number' do
+    (params[:number].to_i ** 2).to_s
+  end
+
+  get '/say/:number/:phrase' do
+    string = params[:phrase].split("%20").join(" ")
+    string *= params[:number].to_i
+  end
+
+  get '/say/:word1/:word2/:word3/:word4/:word5' do
+    string = ""
+    params.each do |key, value|
+      string += value + " "
+    end
+    string.chop + "."
+  end
+
+  get '/:operation/:number1/:number2' do
+    case params[:operation]
+    when "add"
+      (params[:number1].to_i + params[:number2].to_i).to_s
+    when "subtract"
+      (params[:number1].to_i - params[:number2].to_i).to_s
+    when "multiply"
+      (params[:number1].to_i * params[:number2].to_i).to_s
+    when "divide"
+      (params[:number1].to_i / params[:number2].to_i).to_s
+    end
+  end
 end
