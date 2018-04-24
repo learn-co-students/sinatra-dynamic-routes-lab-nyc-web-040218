caf6d4d584a943ef3f66374400756be114da07cc
yongnicholaskim <yongnicholaskim@gmail.com>
2018-04-23 21:30:46 -0400

    Done.

--- app.rb
+++ app.rb
@@ -32,11 +32,15 @@ class App < Sinatra::Base
     @operation = params[:operation]
     @number1 = params[:number1]
     @number2 = params[:number2]
-    # binding.pry
-    "#{@number1.to_i + @number2.to_i}" if @operation == 'add'
-    "#{@number1.to_i - @number2.to_i}" if @operation == 'subtract'
-    "#{@number1.to_i * @number2.to_i}" if @operation == 'multiply'
-    "#{@number1.to_i / @number2.to_i}" if @operation == 'divide'
+    if @operation == 'add'
+      "#{@number1.to_i + @number2.to_i}" 
+    elsif @operation == 'subtract'
+      "#{@number1.to_i - @number2.to_i}" 
+    elsif @operation == 'multiply'
+      "#{@number1.to_i * @number2.to_i}" 
+    elsif @operation == 'divide'
+      "#{@number1.to_i / @number2.to_i}" 
+    end
   end
 
 end
